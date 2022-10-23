fc7a26afa813ac15d86ecec1d78583d44bcc0dbe
tuliosilvacardoso <113935524+tuliosilvacardoso@users.noreply.github.com>
2022-10-23 15:42:22 -0300

    Add files via upload
    [AP-1.2] Testando o Corretor Automático da disciplina BCC701

---
+++ [AP-1.2] Questão 1.py
@@ -0,0 +1,8 @@
+k = float(input("Constante elástica da mola: "))
+x = float(input("Extensão da mola: "))
+m = float(input("Massa do corpo: "))
+F = k*x
+a = F/m
+
+
+print(f"Aceleração do corpo: {a} ")
---
+++ [AP-1.2] Questão 2.py
@@ -0,0 +1,6 @@
+g = 9.807
+t = float(input('Tempo decorrido: '))
+v = g * t
+s = (g * t ** 2) / 2
+print(f'Velocidade: {v}')
+print(f'Distância: {s}')
