590a35987c9b4e7126c72ce6ab678943b99261c5
Tulio Silva <113935524+tuliosilvacardoso@users.noreply.github.com>
2022-10-23 16:12:49 -0300

    Update README.py

--- README.py
+++ README.py
@@ -1,2 +1,1 @@
-# BCC701-Corretor-automatico-Programacao-de-Computadores
-print("Todos os códigos com nota 10 no corretor automático do DECOM da UFOP para a disciplina BCC701 - Programação de Computadores 1")
+# Todos os códigos com nota 10 no corretor automático do DECOM da UFOP para a disciplina BCC701 - Programação de Computadores 1 #
